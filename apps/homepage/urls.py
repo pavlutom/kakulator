0ceccc549894dfae8d98e2f24e4d5004cea7eb7f
Tomáš Pavlůsek <pavlutom@fit.cvut.cz>
2021-03-14 11:36:00 +0100

    simple navbar

--- apps/homepage/urls.py
+++ apps/homepage/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 app_name = "homepage"
 
-urlpatterns = [path("", views.HomeView.as_view(), name="home")]
+urlpatterns = [
+    path("", views.HomeView.as_view(), name="home"),
+    path("about/", views.AboutView.as_view(), name="about"),
+]
--- apps/homepage/views.py
+++ apps/homepage/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "homepage/home.html"
+
+
+class AboutView(TemplateView):
+    template_name = "homepage/about.html"
