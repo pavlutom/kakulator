96aafd4ebae008343dd7d80642bb7d29dfc897c6
Tomáš Pavlůsek <pavlutom@fit.cvut.cz>
2021-03-14 13:22:45 +0100

    simple integer kakulation

--- apps/kakulator/kakulate.py
+++ apps/kakulator/kakulate.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 import random
+import re
 
 
 from .exceptions import KakulationError
@@ -8,14 +9,24 @@ from .exceptions import KakulationError
 
 def kakulate(query):
     if isinstance(query, str):
-        json_path = (
-            Path(__file__).resolve().parent / "resources" / "kakulation_results.json"
-        )
-        with open(json_path) as results_json:
-            results = json.loads(results_json.read())
-            kakulated_result = results[random.randint(0, len(results) - 1)]
-        return kakulated_result
+        if re.match(r"^[ 0-9/*+-]*$", query):
+            return _kakulate_int(random.randint(1, 10))
+        return _kakulate_string(query)
     if isinstance(query, int):
-        return random.randint(0, 10000)
+        return _kakulate_int(query)
 
     raise KakulationError(f"Unable to kakulate object of type '{type(query)}'.")
+
+
+def _kakulate_string(query):
+    json_path = (
+        Path(__file__).resolve().parent / "resources" / "kakulation_results.json"
+    )
+    with open(json_path) as results_json:
+        results = json.loads(results_json.read())
+        kakulated_result = results[random.randint(0, len(results) - 1)]
+    return kakulated_result
+
+
+def _kakulate_int(query):
+    return random.randint(-1000000, 1000000)
